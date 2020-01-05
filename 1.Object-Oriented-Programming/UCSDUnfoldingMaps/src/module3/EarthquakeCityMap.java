900fa682df6d01a28cc864e4bff0035fb9e22e45
Andrew Goddard <anders@campaignforalcoholfree.com>
2020-01-05 07:51:20 +0000

    Updated to use Microsoft map, with the Google one commented out - I prefer the look of the Google one but there are very tight restrictions on the amount it can be called.

--- 1.Object-Oriented-Programming/UCSDUnfoldingMaps/src/module3/EarthquakeCityMap.java
+++ 1.Object-Oriented-Programming/UCSDUnfoldingMaps/src/module3/EarthquakeCityMap.java
@@ -15,6 +15,7 @@ import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
@@ -58,7 +59,8 @@ public class EarthquakeCityMap extends PApplet {
 		    earthquakesURL = "2.5_week.atom"; 	// Same feed, saved Aug 7, 2015, for working offline
 		}
 		else {
-			map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
+			//map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider()); // Can only be used 5 times a day
+			 map = new UnfoldingMap(this, 200, 50, 700, 500, new Microsoft.HybridProvider());
 			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
 			//earthquakesURL = "2.5_week.atom";
 		}
@@ -136,4 +138,9 @@ public class EarthquakeCityMap extends PApplet {
 		// Remember you can use Processing's graphics methods here
 	
 	}
+	
+	public static void main (String... args) {
+        EarthquakeCityMap ecm = new EarthquakeCityMap();
+        PApplet.runSketch(new String[]{"EarthquakeCityMap"}, ecm);
+    }
 }
